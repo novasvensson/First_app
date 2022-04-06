cf69041d31f75b42e5e52859bec3d61d969f5d8c
nova <nova.a.svensson@gmail.com>
2022-04-06 11:58:01 +0200

    Funkar i emulatorn

--- app/src/main/java/com/example/first_app/Compass.java
+++ app/src/main/java/com/example/first_app/Compass.java
@@ -8,11 +8,19 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Compass extends AppCompatActivity implements SensorEventListener {
+
+    ImageView compassImage;
+    ImageView cheese;
+    TextView textView;
+    Vibrator vibrator;
 
     private SensorManager sensorManager;
     private Sensor magSensor;
@@ -22,19 +30,17 @@ public class Compass extends AppCompatActivity implements SensorEventListener {
     boolean haveSensor = false;
     boolean haveSensor2 = false;
 
-    private ImageView compassImage;
-    private ImageView cheese;
-    TextView textView;
-
     int azimuth;
 
-    float[] lastAcc = new float[3];
-    float[] lastMag = new float[3];
-    private float[] orientation = new float[3];
-    private float[] rotMatrix = new float[9];
+    private float[] lastAcc = new float[3];
+    private float[] lastMag = new float[3];
+    float[] orientation = new float[3];
+    float[] rotMatrix = new float[9];
 
     private boolean lastAccSet = false;
     private boolean lastMagSet = false;
+
+    // static final float LOWPASS_FACTOR = 0.25f;// Used for low pass filtering
 
 
     // executing at the App startup
@@ -47,6 +53,7 @@ public class Compass extends AppCompatActivity implements SensorEventListener {
         cheese = (ImageView) findViewById(R.id.imageView3);
         textView = (TextView) findViewById(R.id.textViewCompass);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
         start();
     }
@@ -102,82 +109,76 @@ public class Compass extends AppCompatActivity implements SensorEventListener {
         stop();
     }
 
-    SensorEventListener sensorEventListenerAccelerometer = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent sensorEvent) {
-            if(sensorEvent.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-                SensorManager.getRotationMatrixFromVector(rotMatrix, sensorEvent.values);
-                azimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rotMatrix, orientation)[0]) + 360) % 360;
-            }
-
-            if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                System.arraycopy(sensorEvent.values, 0, lastAcc, 0, sensorEvent.values.length);
-                lastAccSet = true;
-            } else if(sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                System.arraycopy(sensorEvent.values, 0, lastMag, 0, sensorEvent.values.length);
-                lastMagSet = true;
-            }
-
-            if(lastAccSet && lastMagSet) {
-                SensorManager.getRotationMatrix(rotMatrix, null, lastAcc, lastMag);
-                SensorManager.getOrientation(rotMatrix, orientation);
-                azimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rotMatrix, orientation)[0]) + 360) % 360;
-            }
-
-            azimuth = Math.round(azimuth);
-            compassImage.setRotation(-azimuth);
-
-            String where = "NW";
-            float i = 255;
-
-            if(azimuth >= 350 || azimuth <=10) {
-                where = "N";
-                i = 255;
-            }
-            if(azimuth < 350 && azimuth > 280) {
-                where = "NW";
-                i = 255;
-            }
-            if(azimuth <= 280 && azimuth > 260) {
-                where = "W";
-                i = 255;
-            }
-            if(azimuth <= 260 && azimuth > 190) {
-                where = "SW";
-                i = 255;
-            }
-            if(azimuth <= 190 && azimuth > 170) {
-                where = "S";
-                i = 255;
-            }
-            if(azimuth <= 170 && azimuth > 100) {
-                where = "SE";
-                i = 0;
-            }
-            if(azimuth <= 100 && azimuth > 80) {
-                where = "E";
-                i = 0;
-            }
-            if(azimuth <= 80 && azimuth > 10) {
-                where = "NE";
-                i = 0;
-            }
-
-            String text = azimuth + "° " + where;
-            textView.setText(text);
-            cheese.setImageAlpha((int) i);
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int i) {
-
-        }
-    };
-
     // executed when a sensor change its state
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rotMatrix, sensorEvent.values);
+            azimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rotMatrix, orientation)[0]) + 360) % 360;
+        }
 
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(sensorEvent.values, 0, lastAcc, 0, sensorEvent.values.length);
+            // System.arraycopy(lowPass(event.values, mLastAccelerometer), 0, mLastAccelerometer, 0, event.values.length);
+            lastAccSet = true;
+        } else if(sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(sensorEvent.values, 0, lastMag, 0, sensorEvent.values.length);
+            // System.arraycopy(lowPass(event.values, mLastMagnetometer), 0, mLastMagnetometer, 0, event.values.length);
+            lastMagSet = true;
+        }
+
+        if(lastAccSet && lastMagSet) {
+            SensorManager.getRotationMatrix(rotMatrix, null, lastAcc, lastMag);
+            SensorManager.getOrientation(rotMatrix, orientation);
+            azimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rotMatrix, orientation)[0]) + 360) % 360;
+        }
+
+        azimuth = Math.round(azimuth);
+        compassImage.setRotation(-azimuth);
+
+        String where = "NV";
+        float i = 0;
+
+        if(azimuth >= 350 || azimuth <=10) {
+            where = "N";
+            i = 0;
+            if (Build.VERSION.SDK_INT >= 26) {
+                vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                vibrator.vibrate(200);
+            }
+        }
+        if(azimuth < 350 && azimuth > 280) {
+            where = "NV";
+            i = 0;
+        }
+        if(azimuth <= 280 && azimuth > 260) {
+            where = "V";
+            i = 0;
+        }
+        if(azimuth <= 260 && azimuth > 190) {
+            where = "SV";
+            i = 0;
+        }
+        if(azimuth <= 190 && azimuth > 170) {
+            where = "S";
+            i = 0;
+        }
+        if(azimuth <= 170 && azimuth > 100) {
+            where = "SO";
+            i = 255;
+        }
+        if(azimuth <= 100 && azimuth > 80) {
+            where = "Ö";
+            i = 255;
+        }
+        if(azimuth <= 80 && azimuth > 10) {
+            where = "NO";
+            i = 255;
+        }
+
+        textView.setText(azimuth + "° " + where);
+        cheese.setImageAlpha((int) i);
     }
 
     // executed when the sensor change accuracy
